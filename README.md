6e27c7885b0eeb3a841948efb8ea450ef919d264
Richard Vanhook <richard.vanhook@gmail.com>
2011-12-07 20:03:36 -0600

    1.0.1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Basic Usage
 
-  use OmniAuth::Builder do
-    provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
-  end
+use OmniAuth::Builder do
+  provider :salesforce, ENV['SALESFORCE_KEY'], ENV['SALESFORCE_SECRET']
+end
--- lib/omniauth/strategies/salesforce.rb
+++ lib/omniauth/strategies/salesforce.rb
@@ -3,12 +3,29 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Salesforce < OmniAuth::Strategies::OAuth2
+
+      MOBILE_USER_AGENTS =  'webos|ipod|iphone|mobile'
+
       option :client_options, {
         :site          => 'https://login.salesforce.com',
         :authorize_url => '/services/oauth2/authorize',
         :token_url     => '/services/oauth2/token'
       }
+      option :authorize_options, [
+        :scope,
+        :display,
+        :immediate,
+        :state
+      ]
+
       def request_phase
+        req = Rack::Request.new(@env)
+        options.update(req.params)
+        ua = req.user_agent.to_s
+        if !options.has_key?(:display)
+          mobile_request = ua.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
+          options[:display] = mobile_request ? 'touch' : 'page'
+        end
         super
       end
 
@@ -23,15 +40,9 @@ module OmniAuth
           'last_name'       => raw_info['last_name'],
           'location'        => '',
           'description'     => '',
-          'image'           => raw_info['nick_name'],
+          'image'           => raw_info['photos']['thumbnail'] + "?oauth_token=#{access_token.token}",
           'phone'           => '',
-          'urls'            => raw_info['urls'],
-          'organizationid'  => raw_info['organization_id'],
-          'userid'          => raw_info['user_id'],
-          'username'        => raw_info['username'],
-          'organization_id' => raw_info['organization_id'],
-          'user_id'         => raw_info['user_id'],
-          'user_name'       => raw_info['username']
+          'urls'            => raw_info['urls']
         }
       end
 
@@ -40,6 +51,22 @@ module OmniAuth
         access_token.options[:param_name] = :oauth_token
         @raw_info ||= access_token.post(access_token['id']).parsed
       end
+
+      extra do
+        raw_info.merge({
+          'instance_url' => access_token.params['instance_url'],
+          'pod' => access_token.params['instance_url']
+        })
+      end
+      
     end
+
+    class SalesforceSandbox < OmniAuth::Strategies::Salesforce
+      default_options[:client_options][:site] = 'https://test.salesforce.com'
+    end
+    class DatabaseDotCom < OmniAuth::Strategies::Salesforce
+      default_options[:client_options][:site] = 'https://login.database.com'
+    end
+
   end
 end
