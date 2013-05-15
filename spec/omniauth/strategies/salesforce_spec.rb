54b0593dee41638f3e05a091d4bbbda3cf084376
Chulki Lee <chulki@asperasoft.com>
2013-05-15 10:04:19 -0700

    Clean up specs

--- spec/omniauth/strategies/salesforce_spec.rb
+++ spec/omniauth/strategies/salesforce_spec.rb
@@ -1,28 +1,52 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::Salesforce do
-  strategy = nil
-  before do
-    OmniAuth.config.test_mode = true
-    rack_app = []
-    rack_app.stub :call
-    strategy = OmniAuth::Strategies::Salesforce.new rack_app, 'Consumer Key', 'Consumer Secret'
+  let(:rack_app) do
+    rack_app = double('rack_app')
+    rack_app.stub(:call)
+    rack_app
   end
+  let(:strategy) { OmniAuth::Strategies::Salesforce.new rack_app, 'Consumer Key', 'Consumer Secret' }
+  let(:access_token) { OAuth2::AccessToken.from_hash client, access_token_params }
+  let(:access_token_params) do
+    {
+      'id' => client_id,
+      'access_token' => client_id,
+      'instance_url' => instance_url,
+      'signature' => signature,
+      'issued_at' => issued_at
+    }
+  end
+  let(:client_id) { "https://login.salesforce.com/id/00Dd0000000d45TEBQ/005d0000000fyGPCCY" }
+  let(:signature) { Base64.strict_encode64(OpenSSL::HMAC.digest('sha256', strategy.options.client_secret, client_id + issued_at)) }
+  let(:instance_url) { 'http://instance.salesforce.example' }
+  let(:issued_at) { '1296458209517' }
+
+  subject { strategy }
+
+  before(:each) { OmniAuth.config.test_mode = true }
+
   describe "request_phase" do
-    env = nil
-    before do
+    let(:env) do
       env = {
         'rack.session' => {},
         'HTTP_USER_AGENT' => 'unknown',
         'REQUEST_METHOD' => 'GET',
         'rack.input' => '',
         'rack.url_scheme' => 'http',
-        'SERVER_NAME' => 'server.example', 
-        'QUERY_STRING' => 'code=xxxx', 
-        'SCRIPT_NAME' => '', 
+        'SERVER_NAME' => 'server.example',
+        'QUERY_STRING' => 'code=xxxx',
+        'SCRIPT_NAME' => '',
         'SERVER_PORT' => 80
       }
     end
+
+    before(:each) do
+      env['HTTP_USER_AGENT'] = agent
+      strategy.call!(env)
+      strategy.request_phase
+    end
+
     context "when using a mobile browser" do
       user_agents = {
         :Pre => "Mozilla/5.0 (webOS/1.4.0; U; en-US) AppleWebKit/532.2 (KHTML, like Gecko) Version/1.0 Safari/532.2 Pre/1.1",
@@ -36,19 +60,14 @@ describe OmniAuth::Strategies::Salesforce do
         :generic_mobile => "some mobile device"
       }
       user_agents.each_pair do |name, agent|
-        context "with the user agent from a #{name.to_s}" do
-          before do
-            env['HTTP_USER_AGENT'] = agent
-            strategy.call!(env)
-            strategy.request_phase
-          end
+        context "with the user agent #{name.to_s}" do
+          let(:agent) { agent }
           subject {strategy.options}
-          it "sets the :display option to 'touch'" do
-            subject[:display].should == 'touch'
-          end
+          its([:display]) { should == 'touch' }
         end
       end
     end
+
     context "when using a desktop browser" do
       user_agents = {
         :Chrome => "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21",
@@ -57,24 +76,17 @@ describe OmniAuth::Strategies::Salesforce do
         :anything_else => "unknown"
       }
       user_agents.each_pair do |name, agent|
-        context "with the user agent from #{name.to_s}" do
-          before do
-            env['HTTP_USER_AGENT'] = agent
-            strategy.call!(env)
-            strategy.request_phase
-          end
+        context "with the user agent #{name.to_s}" do
+          let(:agent) { agent }
           subject {strategy.options}
-          it "sets the :display option to 'page'" do
-            subject[:display].should == 'page'
-          end
+          its([:display]) { should == 'page' }
         end
       end
     end
   end
   describe "callback phase" do
-    raw_info = nil
-    before do
-      raw_info = {
+    let(:raw_info) do
+      {
         'id' => 'salesforce id',
         'display_name' => 'display name',
         'email' => 'email',
@@ -87,126 +99,73 @@ describe OmniAuth::Strategies::Salesforce do
           "metadata" => "https://salesforce.example/services"
         }
       }
-      client = OAuth2::Client.new 'id', 'secret', {:site => 'example.com'}
-      access_token = OAuth2::AccessToken.from_hash client, {
-        'access_token' => 'token',
-        'instance_url' => 'http://instance.salesforce.example',
-        'signature' => 'invalid',
-        'issued_at' => '1296458209517'
-      }
+    end
+    let(:client) { OAuth2::Client.new 'id', 'secret', {:site => 'example.com'} }
+
+    before(:each) do
       strategy.stub(:raw_info) { raw_info }
       strategy.stub(:access_token) { access_token }
     end
+
     describe "uid" do
-      it "sets the id" do
-        strategy.uid.should == raw_info['id']
-      end
+      its(:uid) { should == raw_info['id'] }
     end
+
     describe "info" do
       subject { strategy.info }
-      it "returns an info hash" do
-        subject.should_not be_nil
-      end
-      it "sets name" do 
-        subject['name'].should == raw_info['display_name']
-      end
-      it "sets email" do
-        subject['email'].should == raw_info['email']
-      end
-      it "sets nickname" do
-        subject['nickname'].should == raw_info['nick_name']
-      end
-      it "sets first_name" do
-        subject['first_name'].should == raw_info['first_name']
-      end
-      it "sets last_name" do
-        subject['last_name'].should == raw_info['last_name']
-      end
-      it "sets location" do
-        subject['location'].should == ''
-      end
-      it "sets description" do
-        subject['description'].should == ''
-      end
-      it "sets image" do
-        subject['image'].should == raw_info['photos']['thumbnail'] + "?oauth_token=#{strategy.access_token.token}"
-      end
-      it "sets phone" do
-        subject['phone'].should == ''
-      end
-      it "sets urls" do
-        subject['urls'].should == raw_info['urls']
-      end
+
+      it { should_not be_nil }
+      its(['name']) { should == raw_info['display_name'] }
+      its(['email']) { should == raw_info['email'] }
+      its(['nickname']) { should == raw_info['nick_name'] }
+      its(['first_name']) { should == raw_info['first_name'] }
+      its(['last_name']) { should == raw_info['last_name'] }
+      its(['first_name']) { should == raw_info['first_name'] }
+      its(['location']) { should == '' }
+      its(['description']) { should == '' }
+      its(['image']) { should == raw_info['photos']['thumbnail'] + "?oauth_token=#{strategy.access_token.token}" }
+      its(['phone']) { should == '' }
+      its(['urls']) { should == raw_info['urls'] }
     end
+
     describe "credentials" do
       subject { strategy.credentials }
-      it "sets token" do 
-        subject['token'].should == strategy.access_token.token
-      end
-      it "sets instance_url" do
-        subject['instance_url'].should == strategy.access_token.params["instance_url"]
-      end
+
+      its(['token']) { should == strategy.access_token.token }
+      its(['instance_url']) { should == strategy.access_token.params['instance_url'] }
+
       context "given a refresh token" do
-        it "sets refresh_token" do
-          subject['refresh_token'].should == strategy.access_token.refresh_token
-        end
+        let(:access_token) { OAuth2::AccessToken.from_hash client, {'refresh_token' => 'refreash_token'} }
+
+        its(['refresh_token']) { should == access_token.refresh_token }
+        its(['refresh_token']) { should_not be_empty }
       end
+
       context "when not given a refresh token" do
-        it "does not set a refresh token" do
-          subject['refresh_token'].should be_nil
-        end
+        its(['refresh_token']) { should be_nil }
       end
     end
+
     describe "extra" do
       subject { strategy.extra }
-      it "sets instance_url" do
-        subject['instance_url'].should == strategy.access_token.params['instance_url']
-      end
-      it "sets pod" do
-        subject['pod'].should == strategy.access_token.params['instance_url']
-      end
-      it "sets signature" do
-        subject['signature'].should == strategy.access_token.params['signature']
-      end
-      it "sets issued_at" do
-        subject['issued_at'].should == strategy.access_token.params['issued_at']
-      end
+
+      its(['instance_url']) { should == access_token.params['instance_url'] }
+      its(['pod']) { should == access_token.params['instance_url'] }
+      its(['signature']) { should == access_token.params['signature'] }
+      its(['issued_at']) { should == access_token.params['issued_at'] }
     end
+
     describe "user id validation" do
-      client_id = nil
-      issued_at = nil
-      signature = nil
-      instance_url = 'http://instance.salesforce.example'
-      before do
-          client_id = "https://login.salesforce.com/id/00Dd0000000d45TEBQ/005d0000000fyGPCCY"
-          issued_at = "1331142541514"
-          signature = Base64.strict_encode64(OpenSSL::HMAC.digest('sha256', strategy.options.client_secret, client_id + issued_at))
-      end
       context "when the signature does not match" do
-        before do
-          access_token = OAuth2::AccessToken.from_hash strategy.access_token.client, {
-            'id' => 'forged client id',
-            'issued_at' => issued_at,
-            'instance_url' => 'http://instance.salesforce.example',
-            'signature' => signature
-          }
-          strategy.stub(:access_token) { access_token }
-        end
+        let(:access_token) { OAuth2::AccessToken.from_hash client, access_token_params.merge('id' => 'forged client id') }
+
         it "should call fail!" do
           strategy.should_receive(:fail!)
           strategy.auth_hash
         end
       end
+
       context "when the signature does match" do
-        before do
-          access_token = OAuth2::AccessToken.from_hash strategy.access_token.client, {
-            'id' => client_id,
-            'issued_at' => issued_at,
-            'instance_url' => 'http://instance.salesforce.example',
-            'signature' => signature
-          }
-          strategy.stub(:access_token) { access_token }
-        end
         it "should not fail" do
           strategy.should_not_receive(:fail!)
           strategy.auth_hash
